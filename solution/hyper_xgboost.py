e94c6e58555a089e8ac95daee9a93d1404521f20
WLM1ke <wlmike@gmail.com>
2019-01-25 14:08:03 +0300

    Подбор параметров xgboost

---
+++ solution/hyper_xgboost.py
@@ -0,0 +1,90 @@
+"""Optimize hyperparameters for catboost"""
+
+import numpy as np
+import pandas as pd
+import xgboost
+import hyperopt
+from hyperopt import hp
+
+import sys
+
+__DATA_VER__ = "0.6"
+VALIDATION_RANGE = 1
+
+PROCESSED_PATH = f"./processed_data/data_{__DATA_VER__}.pickle"
+
+SPACE = dict(
+    tree_method="gpu_hist",
+    silent=True,
+    random_state=284704,
+    learning_rate=0.3,
+    n_estimators=200,
+    max_depth=hp.choice("depth", list(range(1, 17))),
+    min_child_weight=hp.loguniform("l2_leaf_reg", np.log(1), np.log(10000)),
+)
+
+if sys.platform == "darwin":
+    del SPACE["tree_method"]
+
+
+def get_processed_data():
+    return pd.read_pickle(PROCESSED_PATH)
+
+
+def get_train_val_split():
+    data = get_processed_data()
+    test_date_block = data.date_block_num.max()
+
+    x_train = data[data.date_block_num < test_date_block - VALIDATION_RANGE].drop("y", axis=1)
+    y_train = data.loc[data.date_block_num < test_date_block - VALIDATION_RANGE, "y"]
+
+    mask = (test_date_block - VALIDATION_RANGE <= data.date_block_num) & (data.date_block_num < test_date_block)
+    x_valid = data[mask].drop("y", axis=1)
+    y_valid = data.loc[mask, "y"]
+
+    return x_train, y_train, x_valid, y_valid
+
+
+class XGBoostHyper:
+    def __init__(self, train, valid):
+        self._train = train
+        self._valid = valid
+        self._counter = 0
+        self._rmse = None
+        self._clf = None
+
+    def __call__(self, params):
+        clf = xgboost.XGBRegressor(**params)
+        clf.fit(
+            *self._train,
+            eval_metric="rmse",
+            early_stopping_rounds=20,
+            eval_set=[self._valid],
+            verbose=False
+        )
+        rmse = clf.get_best_score()["validation_0"]["rmse"]
+        print(f"Run {self._counter}: "
+              f"RMSE - {rmse:0.5f}"
+              )
+        self._counter += 1
+        if self._rmse is None or rmse < self._rmse:
+            self._rmse = rmse
+            self._clf = clf
+            print(f"Best run: params - {clf.get_params()}")
+            print()
+        return rmse
+
+    @property
+    def best_clf(self):
+        return self._clf
+
+
+if __name__ == '__main__':
+    x_train, y_train, x_valid, y_valid = get_train_val_split()
+
+    opt = XGBoostHyper((x_train, y_train), (x_valid, y_valid))
+    hyperopt.fmin(
+        opt,
+        space=SPACE,
+        algo=hyperopt.tpe.suggest,
+        max_evals=100)
