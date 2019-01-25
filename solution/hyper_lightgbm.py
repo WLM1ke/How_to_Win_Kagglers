e2e4ab888fd0ebcb2c7f0cff245c32a99c97da58
WLM1ke <wlmike@gmail.com>
2019-01-25 17:07:22 +0300

    Предварительная версия с lightgbm

--- solution/hyper_lightgbm.py
+++ solution/hyper_lightgbm.py
@@ -1,8 +1,8 @@
-"""Optimize hyperparameters for xgboost"""
+"""Optimize hyperparameters for lightgbm"""
 
 import numpy as np
 import pandas as pd
-import xgboost
+import lightgbm
 import hyperopt
 from hyperopt import hp
 
@@ -14,17 +14,14 @@ VALIDATION_RANGE = 1
 PROCESSED_PATH = f"./processed_data/data_{__DATA_VER__}.pickle"
 
 SPACE = dict(
-    tree_method="gpu_hist",
     silent=True,
     random_state=284704,
     learning_rate=0.3,
     n_estimators=200,
-    max_depth=hp.choice("depth", list(range(1, 17))),
-    min_child_weight=hp.loguniform("l2_leaf_reg", np.log(1), np.log(10000)),
+    max_depth=hp.choice("depth", list(range(-1, 32))),
+    num_leaves=hp.loguniform("l2_leaf_reg", np.log(2), np.log(10000)),
+    cat=hp.choice("cat", list(range(2)))
 )
-
-if sys.platform == "darwin":
-    del SPACE["tree_method"]
 
 
 def get_processed_data():
@@ -45,7 +42,7 @@ def get_train_val_split():
     return x_train, y_train, x_valid, y_valid
 
 
-class XGBoostHyper:
+class LightGBMHyper:
     def __init__(self, train, valid):
         self._train = train
         self._valid = valid
@@ -54,21 +51,26 @@ class XGBoostHyper:
         self._clf = None
 
     def __call__(self, params):
-        clf = xgboost.XGBRegressor(**params)
+        params["num_leaves"] = int(params["num_leaves"])
+        cat = params.pop("cat")
+        clf = lightgbm.LGBMRegressor(**params)
+        cat_columns = [i for i, col in enumerate(x_train) if not issubclass(x_train[col].dtype.type, np.floating)]
         clf.fit(
             *self._train,
-            eval_metric="rmse",
             early_stopping_rounds=20,
             eval_set=[self._valid],
-            verbose=False
+            verbose=False,
+            categorical_feature=cat_columns if cat else 'auto'
         )
-        rmse = min(clf.evals_result()["validation_0"]["rmse"])
+
+        rmse = clf.best_score_["valid_0"]["l2"] ** 0.5
         print(f"Run {self._counter}: RMSE - {rmse:0.5f}")
         self._counter += 1
         if self._rmse is None or rmse < self._rmse:
             self._rmse = rmse
             self._clf = clf
             print(f"Best run: params - {clf.get_params()}")
+            print(f"{'Cat used' if cat else 'Cat not used'}")
             print()
         return rmse
 
@@ -80,7 +82,7 @@ class XGBoostHyper:
 if __name__ == '__main__':
     x_train, y_train, x_valid, y_valid = get_train_val_split()
 
-    opt = XGBoostHyper((x_train, y_train), (x_valid, y_valid))
+    opt = LightGBMHyper((x_train, y_train), (x_valid, y_valid))
     hyperopt.fmin(
         opt,
         space=SPACE,
